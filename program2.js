21f43af2f0dd482397bd11047ffa4666482fa887
Reet Sahu <124372907+Reetsahu20@users.noreply.github.com>
2024-02-23 13:43:46 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,29 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (i > 0 && romanValues[s[i]] > romanValues[s[i - 1]]) {
+            result += romanValues[s[i]] - 2 * romanValues[s[i - 1]];
+        } else {
+            result += romanValues[s[i]];
+        }
+    }
+
+    return result;
+}
 
 
-module.exports={romanToInt}
+console.log(romanToInt("III"));     
+console.log(romanToInt("LVIII"));    
+console.log(romanToInt("MCMXCIV"));
+console.log(romanToInt("aASZQsss"));  
